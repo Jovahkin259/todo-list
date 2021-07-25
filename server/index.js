2820fa7ee0ca0070477d44bc34dc58a157ddbf70
Joshua Astle <jovahkin259@gmail.com>
2021-07-25 17:03:57 +0100

    Add dotenv

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config;
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
